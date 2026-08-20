0d887958a1131fc29bb71791ce5e24ef8a736c85
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-08-21 00:12:24 +0530

    update

--- pattern.java
+++ pattern.java
@@ -34,17 +34,17 @@
 //    }
 // }
 
-public class pattern{
-   public static void main(String[] args) {
-       int n=5;
-       char ch ='A';
+// public class pattern{
+//    public static void main(String[] args) {
+//        int n=5;
+//        char ch ='A';
 
-       for( int line=1;line<=n;line++){
-         for(int chars=1;chars<=line;chars++){
-            System.out.print(ch);
-            ch++;
-         }
-         System.out.println();
-       }
-   }
-}
+//        for( int line=1;line<=n;line++){
+//          for(int chars=1;chars<=line;chars++){
+//             System.out.print(ch);
+//             ch++;
+//          }
+//          System.out.println();
+//        }
+//    }
+// }
