858e4183e3b6e33a15fbc130b95920bd959146ce
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-08-20 23:23:08 +0530

    code update

--- hello.java
+++ hello.java
@@ -9,11 +9,24 @@
 
 //how to use the pattern
 
+// public class hello{
+//     public static void main(String[] args) {
+//         int n=7;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+// System.out.println();
+//         }
+
+//     }
+// }
+
 public class hello{
     public static void main(String[] args) {
         int n=7;
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
+            for(int j=1;j<=(n-i+1);j++){
                 System.out.print("*");
             }
 System.out.println();
