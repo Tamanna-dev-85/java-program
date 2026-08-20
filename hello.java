634e5e2f2f25aee45a0d261516cbd8568c877ef5
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-08-21 00:13:42 +0530

    update

--- hello.java
+++ hello.java
@@ -48,15 +48,15 @@
 //     }
 // }
 
-public class hello{
-    public static void main(String[] args) {
-        int n=7;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=(n-i+1);j++){
-                System.out.print(j);
-            }
-System.out.println();
-        }
+// public class hello{
+//     public static void main(String[] args) {
+//         int n=7;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=(n-i+1);j++){
+//                 System.out.print(j);
+//             }
+// System.out.println();
+//         }
 
-    }
-}
+//     }
+// }
