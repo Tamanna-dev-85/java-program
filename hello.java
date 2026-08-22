ae6508102821fb4f62510d07b696cea748b7185c
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-08-22 20:46:43 +0530

    new update

--- hello.java
+++ hello.java
@@ -77,8 +77,11 @@ public class hello {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        int a=sc.nextInt();
+        int b=sc.nextInt();
 
-        int num=sc.nextInt();
+        int num=a+b;
         System.out.println(num);
     }
 }
