dad88a0aa00592dc4959e2033efaefe427c9f906
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-08-22 20:41:12 +0530

    update code

--- hello.java
+++ hello.java
@@ -60,3 +60,13 @@
 
 //     }
 // }
+
+public class hello {
+
+    public static void main(String[] args) {
+        int a=3;
+        int b=2;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
