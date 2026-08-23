7ff3b8d19218291cb35009146e247092f0245eb8
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-08-23 20:22:08 +0530

    update code

--- hello.java
+++ hello.java
@@ -85,3 +85,18 @@
 //         System.out.println(num);
 //     }
 // }
+
+import java.util.*;
+public class hello{
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+          int n = sc.nextInt();
+         int counter = 1;
+       
+         while(counter<=n){
+             System.out.print(counter + " ");
+          counter++;
+         }
+ System.out.println();
+     }
+ }
