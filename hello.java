367e0b7da244c42d58303587746bed1aa5914e21
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-08-24 23:47:27 +0530

    update code

--- hello.java
+++ hello.java
@@ -110,3 +110,22 @@
 //          System.out.println("hw printed");
 //      }
 //  }
+
+public class Opera{
+   public static void main(String[] args) {
+    
+      int number=3;
+
+      switch(number){
+
+        case 1: System.out.println("samosa");
+        break;
+        case 2: System.out.println("pizza");
+        break;
+        case 3: System.out.println("momo");
+        break;
+        default:System.out.println("dreaming");
+
+    }
+  }
+}
