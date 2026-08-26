6c8c0dca4c2b41a308cc8fd6c36989cf0a376ced
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-08-26 21:07:41 +0530

    update

--- hello.java
+++ hello.java
@@ -172,12 +172,28 @@
 //     }
 // }
 
+// public class hello{
+//     public static void main(String[] args) {
+//         int x = 9, y = 12;
+//         int a = 2, b = 4, c = 6;
+
+//         int exp = 4/3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b*y);
+//         System.out.println(exp);
+//     }
+// }
+
 public class hello{
     public static void main(String[] args) {
-        int x = 9, y = 12;
-        int a = 2, b = 4, c = 6;
+        int x = 200, y = 50 ,z=100;
 
-        int exp = 4/3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b*y);
-        System.out.println(exp);
+        if(x>y && y>z){
+            System.out.println("hello");
+        }
+        if(z>y && z<x){
+            System.out.println("java");
+        }
+        if((y+200)< x && (y+150)<z){
+            System.out.println("hello java");
+        }
     }
 }
