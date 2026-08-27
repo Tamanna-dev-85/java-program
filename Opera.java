e2f5b6f3a225a16566a6e736dc1a3ca35edb32e3
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-08-27 23:36:37 +0530

    update code

--- Opera.java
+++ Opera.java
@@ -506,3 +506,18 @@
 //       }
 //   }
 // }
+
+public class Opera {
+    public static void main(String[] args) {
+        int A=10;
+        int B=5;
+        System.out.println("add="+(A+B));
+        System.out.println("sub="+(A-B));
+        System.out.println("mul="+(A*B));
+        System.out.println("div="+(A/B));
+        System.out.println("mod="+(A%B));
+
+
+
+    }
+}
