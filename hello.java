c3f06a2c9e6bd9022a11aceda7bfe4b8410f400a
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-08-27 23:34:29 +0530

    update

--- hello.java
+++ hello.java
@@ -232,17 +232,17 @@
 //     }
 // }
 
-public class hello {
-    public static void main(String[] args) {
-        int A=10;
-        int B=5;
-        System.out.println("add="+(A+B));
-        System.out.println("sub="+(A-B));
-        System.out.println("mul="+(A*B));
-        System.out.println("div="+(A/B));
-        System.out.println("mod="+(A%B));
+// public class hello {
+//     public static void main(String[] args) {
+//         int A=10;
+//         int B=5;
+//         System.out.println("add="+(A+B));
+//         System.out.println("sub="+(A-B));
+//         System.out.println("mul="+(A*B));
+//         System.out.println("div="+(A/B));
+//         System.out.println("mod="+(A%B));
 
 
 
-    }
-}
+//     }
+// }
