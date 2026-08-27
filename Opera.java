8285524a71ee02e24ca136c9516512924c61e128
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-08-27 23:37:14 +0530

    update

--- Opera.java
+++ Opera.java
@@ -507,17 +507,17 @@
 //   }
 // }
 
-public class Opera {
-    public static void main(String[] args) {
-        int A=10;
-        int B=5;
-        System.out.println("add="+(A+B));
-        System.out.println("sub="+(A-B));
-        System.out.println("mul="+(A*B));
-        System.out.println("div="+(A/B));
-        System.out.println("mod="+(A%B));
+// public class Opera {
+//     public static void main(String[] args) {
+//         int A=10;
+//         int B=5;
+//         System.out.println("add="+(A+B));
+//         System.out.println("sub="+(A-B));
+//         System.out.println("mul="+(A*B));
+//         System.out.println("div="+(A/B));
+//         System.out.println("mod="+(A%B));
 
 
 
-    }
-}
+//     }
+// }
