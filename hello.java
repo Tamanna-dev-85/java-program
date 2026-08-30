1af9030ece1858b1e9c0a329a2a1d29b877f421b
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-08-30 23:24:51 +0530

    update

--- hello.java
+++ hello.java
@@ -274,3 +274,14 @@
 //             System.out.println();
 //       }
 // }
+
+import java.util.*;
+public class hello{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int put=sc.nextInt();
+        System.out.println(put);
+    }
+}
+
+
