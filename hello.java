f17e1beab5354daaab36977dc7790dc78c339faa
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-01 22:46:00 +0530

    update code

--- hello.java
+++ hello.java
@@ -304,13 +304,13 @@
 //  }
 // }
 
-import java.util.*;
-public class hello{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int product = a+b;
-        System.out.println(product);
-    }
-}
+// import java.util.*;
+// public class hello{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int product = a+b;
+//         System.out.println(product);
+//     }
+// }
