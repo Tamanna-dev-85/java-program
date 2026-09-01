780b8eca738ec32da07371951084d20fc4540f4f
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-01 23:02:33 +0530

    update

--- Start.java
+++ Start.java
@@ -420,8 +420,8 @@
 
 // import java.util.*;
 // public class Start{
-       // public static void main(String[] args) {
-            //Scanner sc = new Scanner(System.in);
+//        public static void main(String[] args) {
+//             Scanner sc = new Scanner(System.in);
 //             float a = 23.5f;
 //             int b = (int)a;
 //             System.out.println(b);
@@ -507,26 +507,26 @@
 //           }
 //         }
 // }
-import java.util.*;
-public class Start{
-        public static void main(String[] args) {
-                Scanner sc =new Scanner(System.in);
-                int income = sc. nextInt();
-                int tax;
+// import java.util.*;
+// public class Start{
+//         public static void main(String[] args) {
+//                 Scanner sc =new Scanner(System.in);
+//                 int income = sc. nextInt();
+//                 int tax;
           
-          if(income<500000){
-                tax=0;
+//           if(income<500000){
+//                 tax=0;
                
-          }
-          else if(income>=500000 && income<1000000){
-                tax=(int)(income*0.2);
-          }
-          else{
-                tax=(int)(income*0.3);
-          }
-          System.out.println("your tax is:"+tax);
-        }
-}
+//           }
+//           else if(income>=500000 && income<1000000){
+//                 tax=(int)(income*0.2);
+//           }
+//           else{
+//                 tax=(int)(income*0.3);
+//           }
+//           System.out.println("your tax is:"+tax);
+//         }
+// }
 
 // public class Start{
 //         public static void main(String[] args) {
