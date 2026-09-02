378f79807809e845e7ffc412a27420f815c77277
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-02 23:41:46 +0530

    update code

--- hello.java
+++ hello.java
@@ -332,3 +332,17 @@
 
 //         }
 //  }
+
+
+class hello {
+    public static void main(String[] args) {
+        int n = 5;
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println("Factorial = " + fact);
+    }
+}
