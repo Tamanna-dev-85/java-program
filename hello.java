3dce37edf170a5460da21251d86544da5cf53457
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-03 00:19:45 +0530

    update code

--- hello.java
+++ hello.java
@@ -347,15 +347,15 @@
 //     }
 // }
 
-class hello {
-    public static void main(String[] args) {
-        int a = 10, b = 25, c = 15;
+// class hello {
+//     public static void main(String[] args) {
+//         int a = 10, b = 25, c = 15;
 
-        if (a > b && a > c)
-            System.out.println("A is largest");
-        else if (b > c)
-            System.out.println("B is largest");
-        else
-            System.out.println("C is largest");
-    }
-}
+//         if (a > b && a > c)
+//             System.out.println("A is largest");
+//         else if (b > c)
+//             System.out.println("B is largest");
+//         else
+//             System.out.println("C is largest");
+//     }
+//}
