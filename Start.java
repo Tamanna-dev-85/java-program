89eced871643065ee93b3414bd1c7711e4872aff
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-02 23:44:31 +0530

    update code

--- Start.java
+++ Start.java
@@ -587,3 +587,13 @@
 //         System.out.println("Reverse = " + reverse);
 //     }
 // }
+
+class Start{
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+}
