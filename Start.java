4fbc1b53027865cb2b1c553eaac855906a41db73
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-02 23:39:52 +0530

    update code

--- Start.java
+++ Start.java
@@ -573,17 +573,17 @@
 //     }
 // }
 
-class Start{
-    public static void main(String[] args) {
-        int n = 1234;
-        int reverse = 0;
+// class Start{
+//     public static void main(String[] args) {
+//         int n = 1234;
+//         int reverse = 0;
 
-        while (n != 0) {
-            int digit = n % 10;
-            reverse = reverse * 10 + digit;
-            n = n / 10;
-        }
+//         while (n != 0) {
+//             int digit = n % 10;
+//             reverse = reverse * 10 + digit;
+//             n = n / 10;
+//         }
 
-        System.out.println("Reverse = " + reverse);
-    }
-}
+//         System.out.println("Reverse = " + reverse);
+//     }
+// }
