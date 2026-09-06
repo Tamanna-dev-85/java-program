8d665f79a9a60f51f292bd675d7bbb5d1ffc6177
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-06 23:24:32 +0530

    update code

--- loops.java
+++ loops.java
@@ -334,3 +334,18 @@
 //         }
 //     }
 // }
+
+public class loops{
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 4; i++) {
+
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+    }
+}
