42ec60b37189b6121894364f4295d5779274d0ba
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-07 21:58:40 +0530

    update code

--- loops.java
+++ loops.java
@@ -350,3 +350,16 @@
 //     }
 // }
 
+public class loops{
+    public static void main(String[] args) {
+        int n = 12345;
+        int count = 0;
+
+        while (n > 0) {
+            n = n / 10;
+            count++;
+        }
+
+        System.out.println("Digits = " + count);
+    }
+}
