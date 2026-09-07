5283b7e3672b514adc9c0488cffa4229d91942f5
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-07 21:53:26 +0530

    update code

--- Start.java
+++ Start.java
@@ -597,3 +597,17 @@
 //         }
 //     }
 // }
+
+public class loops{
+    public static void main(String[] args) {
+
+        int n = 5;
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println("Factorial = " + fact);
+    }
+}
