cf54760e9d32f98e06739cfe8853a174a6158e2d
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-08 23:37:41 +0530

    update code

--- Basics.java
+++ Basics.java
@@ -157,6 +157,18 @@
 //  }
 // }
 
+import java.util.Scanner;
 
+class Basics{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+}
 
 
