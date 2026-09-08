9c303ac9a6b54dd9ecb8a09736b44fe12335cf44
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-08 23:44:21 +0530

    update code

--- Basics.java
+++ Basics.java
@@ -192,3 +192,17 @@
 //         System.out.println("Sum = " + add(a, b));
 //     }
 // }
+
+import java.util.Scanner;
+
+class Basics{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        System.out.println("You entered: " + num);
+    }
+}
