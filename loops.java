9195636c005bbc20324246fd21360d8b906acc4d
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-08 23:33:52 +0530

    update code

--- loops.java
+++ loops.java
@@ -364,16 +364,17 @@
 //     }
 // }
 
-public class loops{
-    public static void main(String[] args) {
-        int a = 0, b = 1;
+// public class loops{
+//     public static void main(String[] args) {
+//         int a = 0, b = 1;
 
-        for (int i = 1; i <= 10; i++) {
-            System.out.print(a + " ");
+//         for (int i = 1; i <= 10; i++) {
+//             System.out.print(a + " ");
 
-            int c = a + b;
-            a = b;
-            b = c;
-        }
-    }
-}
+//             int c = a + b;
+//             a = b;
+//             b = c;
+//         }
+//     }
+// }
+
