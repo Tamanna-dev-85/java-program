ff0b8f72480b8fb919edce44e36324a69f21437f
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-09 22:45:11 +0530

    update code

--- hello.java
+++ hello.java
@@ -368,28 +368,28 @@
 //                 break;
 //             }
 //           System.out.println(i);  
-//         }
-//         System.out.println("i am out of the loop");
-//     }
-// }
+// //         }
+// //         System.out.println("i am out of the loop");
+// //     }
+// // }
 
-import java.util.*;
-public class hello{
-        public static void main(String[] args) {
-                Scanner sc =new Scanner(System.in);
-                int income = sc. nextInt();
-                int tax;
+// import java.util.*;
+// public class hello{
+//         public static void main(String[] args) {
+//                 Scanner sc =new Scanner(System.in);
+//                 int income = sc. nextInt();
+//                 int tax;
           
-          if(income<500000){
-                tax=0;
+//           if(income<500000){
+//                 tax=0;
                
-          }
-          else if(income>=500000 && income<1000000){
-                tax=(int)(income*0.2);
-          }
-          else{
-                tax=(int)(income*0.3);
-          }
-          System.out.println("your tax is:"+tax);
-        }
-}
+//           }
+//           else if(income>=500000 && income<1000000){
+//                 tax=(int)(income*0.2);
+//           }
+//           else{
+//                 tax=(int)(income*0.3);
+//           }
+//           System.out.println("your tax is:"+tax);
+//         }
+// }
