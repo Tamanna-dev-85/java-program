50e69a70f01f6963d0102b3ef56f204b1db73c64
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-09 22:43:23 +0530

    update code

--- hello.java
+++ hello.java
@@ -360,15 +360,15 @@
 //     }
 //}
 
-public class hello{
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
+// public class hello{
+//     public static void main(String[] args) {
+//         for (int i = 1; i <= 10; i++) {
 
-            if(i==5){
-                break;
-            }
-          System.out.println(i);  
-        }
-        System.out.println("i am out of the loop");
-    }
-}
+//             if(i==5){
+//                 break;
+//             }
+//           System.out.println(i);  
+//         }
+//         System.out.println("i am out of the loop");
+//     }
+// }
