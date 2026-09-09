5f714bf5b8e5b01615cfb23254a07ae91a3e42b8
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-09 22:42:48 +0530

    update code

--- hello.java
+++ hello.java
@@ -359,3 +359,16 @@
 //             System.out.println("C is largest");
 //     }
 //}
+
+public class hello{
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+
+            if(i==5){
+                break;
+            }
+          System.out.println(i);  
+        }
+        System.out.println("i am out of the loop");
+    }
+}
