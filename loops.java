40f51098861db0a928f05fff488e2577d3db84e8
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-14 23:20:07 +0530

    update code

--- loops.java
+++ loops.java
@@ -192,18 +192,18 @@
 //       }
 // }
 
-public class loops{
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
+// public class loops{
+//     public static void main(String[] args) {
+//         for (int i = 1; i <= 10; i++) {
 
-            if(i==5){
-                break;
-            }
-          System.out.println(i);  
-        }
-        System.out.println("i am out of the loop");
-    }
-}
+//             if(i==5){
+//                 break;
+//             }
+//           System.out.println(i);  
+//         }
+//         System.out.println("i am out of the loop");
+//     }
+// }
 
 // import java.util.Scanner;
 
