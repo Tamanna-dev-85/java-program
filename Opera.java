79070be5950dc8543a0c27f83bef3d9066ee27a9
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-16 00:44:43 +0530

    update code

--- Opera.java
+++ Opera.java
@@ -133,23 +133,23 @@
 // }
 
 
-// public class Opera{
-//     public static void main(String[] args) {
-//         int x = 200, y = 50 ,z=100;
+public class Opera{
+    public static void main(String[] args) {
+        int x = 200, y = 50 ,z=100;
 
-//         if(x>y && y>z){
-//             System.out.println("hello");
-//         }
-//         if(z>y && z<x){
-//             System.out.println("java");
-//         }
-//         if((y+200)< x && (y+150)<z){
-//             System.out.println("hello java");
-//         }
+        if(x>y && y>z){
+            System.out.println("hello");
+        }
+        if(z>y && z<x){
+            System.out.println("java");
+        }
+        if((y+200)< x && (y+150)<z){
+            System.out.println("hello java");
+        }
 
 
-//     }
-// }
+    }
+}
 
 // public class Opera {
 //     public static void main(String[] args){
