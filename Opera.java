8826ce096b3c70954d38a5167c9df2a9277a557b
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-16 00:45:30 +0530

    update code

--- Opera.java
+++ Opera.java
@@ -162,15 +162,15 @@
 //}
 //}
 
-// public class Opera{
-//     public static void main(String[] args) {
-//         int x = 9, y = 12;
-//         int a = 2, b = 4, c = 6;
+public class Opera{
+    public static void main(String[] args) {
+        int x = 9, y = 12;
+        int a = 2, b = 4, c = 6;
 
-//         int exp = 4/3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b*y);
-//         System.out.println(exp);
-//     }
-// }
+        int exp = 4/3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b*y);
+        System.out.println(exp);
+    }
+}
 
 
 // public class Opera{
