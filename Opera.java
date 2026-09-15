d6ed1ee6c1444c2501d318fced5b90b115d73202
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-16 00:46:19 +0530

    update code

--- Opera.java
+++ Opera.java
@@ -173,19 +173,19 @@
 // }
 
 
-// public class Opera{
-//     public static void main(String[] args) {
-//         int x=10, y=5;
+public class Opera{
+    public static void main(String[] args) {
+        int x=10, y=5;
 
-//         int exp1 = (y * (x / y + x / y));
-//         int exp2 = (y * x / y + y * x / y);
+        int exp1 = (y * (x / y + x / y));
+        int exp2 = (y * x / y + y * x / y);
 
-//         System.out.println(exp1);
-//         System.out.println(exp2);
+        System.out.println(exp1);
+        System.out.println(exp2);
 
 
-//     }
-// }
+    }
+}
 
 // import java.util.*;
 // public class Opera{
