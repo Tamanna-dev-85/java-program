94886fd86c2ca17ee29c5fac87f36d185c961958
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-15 20:09:46 +0530

    update code

--- loops.java
+++ loops.java
@@ -293,26 +293,26 @@
 
 // n is prime or not prime
 
-public class loops{
-      public static void main(String[] args) {
-          Scanner sc = new Scanner(System.in);
-          int n = sc.nextInt();
-          if(n==2){
-            System.err.println("n is prime");
-          }else{
-          boolean isPrime = true;
-          for(int i=2;i<=n-1;i++){
-            if(n%i==0){
-                  isPrime=false;
-            }
-          }
-          if(isPrime == true){
-            System.err.println("n is prime");
-          }else{
-            System.err.println("n is not prime");
-          }
-      }
-}} 
+// public class loops{
+//       public static void main(String[] args) {
+//           Scanner sc = new Scanner(System.in);
+//           int n = sc.nextInt();
+//           if(n==2){
+//             System.err.println("n is prime");
+//           }else{
+//           boolean isPrime = true;
+//           for(int i=2;i<=n-1;i++){
+//             if(n%i==0){
+//                   isPrime=false;
+//             }
+//           }
+//           if(isPrime == true){
+//             System.err.println("n is prime");
+//           }else{
+//             System.err.println("n is not prime");
+//           }
+//       }
+// }} 
 
 // public class loops {
 //     public static void main(String[] args) {
