5fe5657f25724f4597515f17b899b89335622804
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-18 22:48:17 +0530

    update code

--- hello.java
+++ hello.java
@@ -429,3 +429,21 @@
 //             System.out.println("Not Palindrome");
 //     }
 // }
+class hello{
+    public static void main(String[] args) {
+        int n = 153;
+        int original = n;
+        int sum = 0;
+
+        while (n != 0) {
+            int digit = n % 10;
+            sum += digit * digit * digit;
+            n /= 10;
+        }
+
+        if (sum == original)
+            System.out.println("Armstrong");
+        else
+            System.out.println("Not Armstrong");
+    }
+}
