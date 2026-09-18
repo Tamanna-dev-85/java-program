f732f4a564fd4e41881a7d372ef2e951ce065eda
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-18 22:57:05 +0530

    update code

--- hello.java
+++ hello.java
@@ -447,3 +447,17 @@
 //             System.out.println("Not Armstrong");
 //     }
 // }
+
+class hello{
+    public static void main(String[] args) {
+        int[] arr = {10, 50, 20, 80, 30};
+        int max = arr[0];
+
+        for (int x : arr) {
+            if (x > max)
+                max = x;
+        }
+
+        System.out.println(max);
+    }
+}
