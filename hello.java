65db01c30ccc15c6141792d179579fda802d4171
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-18 22:41:22 +0530

    update code

--- hello.java
+++ hello.java
@@ -412,4 +412,20 @@
 //         }
  //}
 
- 
+ class hello{
+    public static void main(String[] args) {
+        int n = 121;
+        int original = n;
+        int rev = 0;
+
+        while (n != 0) {
+            rev = rev * 10 + n % 10;
+            n /= 10;
+        }
+
+        if (original == rev)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
