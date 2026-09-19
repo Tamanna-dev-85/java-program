f5b5aec0f18fee6619842d7bb28a21714dce9ecf
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-19 16:52:58 +0530

    update code

--- hello.java
+++ hello.java
@@ -508,3 +508,23 @@
 //         System.out.println(add(10, 20, 30));
 //     }
 // }
+
+class Animal {
+    void eat() {
+        System.out.println("Eating");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Barking");
+    }
+}
+
+class hello{
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();
+        d.bark();
+    }
+}
