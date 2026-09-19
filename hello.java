36286bd412a449703a6cc2913d09030c25d90fbd
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-19 16:50:08 +0530

    update code

--- hello.java
+++ hello.java
@@ -492,3 +492,19 @@
 //             System.out.println("Fail");
 //     }
 // }
+
+class hello{
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(10, 20));
+        System.out.println(add(10, 20, 30));
+    }
+}
