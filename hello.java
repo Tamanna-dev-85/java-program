564a9d0c6c7c97c70ad3f45dc6c3737cd7ef419f
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-20 00:08:05 +0530

    update code

--- hello.java
+++ hello.java
@@ -561,16 +561,16 @@
 // }
 
 
-public class hello{
-    public static void main(String[] args) {
-        int n = 10895;
-        int rev = 0;
-        while (n>0) {
-            int lastdigit = n % 10;
-            rev = (rev*10) + lastdigit;
-            n = n/10; 
+// public class hello{
+//     public static void main(String[] args) {
+//         int n = 10895;
+//         int rev = 0;
+//         while (n>0) {
+//             int lastdigit = n % 10;
+//             rev = (rev*10) + lastdigit;
+   //         n = n/10; 
             
-        }
-        System.out.println(rev);
-    }
-}
+  //      }
+    //    System.out.println(rev);
+  //  }
+//}
