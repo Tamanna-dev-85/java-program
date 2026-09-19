9d61bd5e41f6dc1e9bd53a66bb16dbb7daec788a
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-19 23:39:22 +0530

    update code

--- hello.java
+++ hello.java
@@ -537,12 +537,12 @@
 //     }
 // }
 
-public class hello{
-    public static void main(String[] args) {
-        int line = 1;
-        while(line<=4){
-            System.out.println("****");
-            line++;
-        }
-    }
-}
+// public class hello{
+//     public static void main(String[] args) {
+//         int line = 1;
+//         while(line<=4){
+//             System.out.println("****");
+//             line++;
+//         }
+//     }
+// }
