837d81546cb0770cb8465be05bee5b8f2b3b29d8
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-19 23:47:59 +0530

    update code

--- hello.java
+++ hello.java
@@ -546,3 +546,16 @@
 //         }
 //     }
 // }
+
+public class hello{
+    public static void main(String[] args) {
+        int n = 10859;
+        while (n>0) {
+            int lastdigit = n%10;
+            System.out.print(lastdigit+" ");
+            n = n/10; 
+            
+        }
+        System.out.println();
+    }
+}
