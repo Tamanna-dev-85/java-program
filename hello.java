aa11628e843aa206bd0cbddaf270c2665b899ca0
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-19 16:49:37 +0530

    update code

--- hello.java
+++ hello.java
@@ -476,19 +476,19 @@
 //     }
 // }
 
-class Main {
-    public static void main(String[] args) {
-        int marks = 85;
+// class hello{
+//     public static void main(String[] args) {
+//         int marks = 85;
 
-        if (marks >= 90)
-            System.out.println("Grade A+");
-        else if (marks >= 80)
-            System.out.println("Grade A");
-        else if (marks >= 70)
-            System.out.println("Grade B");
-        else if (marks >= 60)
-            System.out.println("Grade C");
-        else
-            System.out.println("Fail");
-    }
-}
+//         if (marks >= 90)
+//             System.out.println("Grade A+");
+//         else if (marks >= 80)
+//             System.out.println("Grade A");
+//         else if (marks >= 70)
+//             System.out.println("Grade B");
+//         else if (marks >= 60)
+//             System.out.println("Grade C");
+//         else
+//             System.out.println("Fail");
+//     }
+// }
