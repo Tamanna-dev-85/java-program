458975492c54b3e1eb5f0c2b29dbd528e9ee1ddb
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-19 16:51:36 +0530

    update code

--- hello.java
+++ hello.java
@@ -493,18 +493,18 @@
 //     }
 // }
 
-class hello{
+// class hello{
 
-    static int add(int a, int b) {
-        return a + b;
-    }
+//     static int add(int a, int b) {
+//         return a + b;
+//     }
 
-    static int add(int a, int b, int c) {
-        return a + b + c;
-    }
+//     static int add(int a, int b, int c) {
+//         return a + b + c;
+//     }
 
-    public static void main(String[] args) {
-        System.out.println(add(10, 20));
-        System.out.println(add(10, 20, 30));
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println(add(10, 20));
+//         System.out.println(add(10, 20, 30));
+//     }
+// }
