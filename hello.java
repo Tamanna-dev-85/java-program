f36a1df0946d154ce098c183aadeac223d4bb853
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-20 23:57:50 +0530

    update code

--- hello.java
+++ hello.java
@@ -618,13 +618,13 @@
 //     }
 // }
 
-class hello{
+// class hello{
 
-    static void add(int a, int b) {
-        System.out.println(a + b);
-    }
+//     static void add(int a, int b) {
+//         System.out.println(a + b);
+//     }
 
-    public static void main(String[] args) {
-        add(10, 20);
-    }
-}
+//     public static void main(String[] args) {
+//         add(10, 20);
+//     }
+// }
