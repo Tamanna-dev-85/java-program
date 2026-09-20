d7d93012b9771d93e9717658fad7a1cfe0809faf
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-20 23:54:07 +0530

    update code

--- hello.java
+++ hello.java
@@ -575,30 +575,30 @@
   //  }
 //}
 
-class hello{
-    public static void main(String[] args) {
-        int a = 20, b = 10;
-        char op = '+';
+// class hello{
+//     public static void main(String[] args) {
+//         int a = 20, b = 10;
+//         char op = '+';
 
-        switch (op) {
-            case '+':
-                System.out.println(a + b);
-                break;
+//         switch (op) {
+//             case '+':
+//                 System.out.println(a + b);
+//                 break;
 
-            case '-':
-                System.out.println(a - b);
-                break;
+//             case '-':
+//                 System.out.println(a - b);
+//                 break;
 
-            case '*':
-                System.out.println(a * b);
-                break;
+//             case '*':
+//                 System.out.println(a * b);
+//                 break;
 
-            case '/':
-                System.out.println(a / b);
-                break;
+//             case '/':
+//                 System.out.println(a / b);
+//                 break;
 
-            default:
-                System.out.println("Invalid Operator");
-        }
-    }
-}
+//             default:
+//                 System.out.println("Invalid Operator");
+//         }
+//     }
+// }
