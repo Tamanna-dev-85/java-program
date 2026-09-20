790bf304719cce6a4ae61aba3264c42fb75d47cc
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-21 00:01:22 +0530

    update code

--- hello.java
+++ hello.java
@@ -628,3 +628,14 @@
 //         add(10, 20);
 //     }
 // }
+
+class hello{
+
+    static int square(int n) {
+        return n * n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(square(5));
+    }
+}
