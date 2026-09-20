2a0b29f07ee4abcabe8fa3aae6b6d18305279821
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-21 00:11:33 +0530

    update code

--- hello.java
+++ hello.java
@@ -664,10 +664,10 @@
 //     }
 // }
 
-class hello{
-    public static void main(String[] args) {
-        String name = "java";
+// class hello{
+//     public static void main(String[] args) {
+//         String name = "java";
 
-        System.out.println(name.toUpperCase());
-    }
-}
+//         System.out.println(name.toUpperCase());
+//     }
+// }
