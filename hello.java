559ecac9ad7b776dde17c5e3bd31895dfbd0120d
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-22 20:13:06 +0530

    update code

--- hello.java
+++ hello.java
@@ -672,15 +672,15 @@
 //     }
 // }
 
-public class hello{
-  public static void main(String args[]){
-    int age = 2;
+// public class hello{
+//   public static void main(String args[]){
+//     int age = 2;
 
-    if(age>=18){
-      System.out.println("adult");
-    }
-  else{
-  System.out.println("not adult");
-  }
-}
-}
+//     if(age>=18){
+//       System.out.println("adult");
+//     }
+//   else{
+//   System.out.println("not adult");
+//   }
+// }
+// }
