810da69783c7e9af5f644b8d2dfbb98c6721ed01
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-22 23:06:37 +0530

    update code

--- Start.java
+++ Start.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 // public class Start{
 //     public static void main (String args[]){
 //         System.out.println("Hello world!");
@@ -622,3 +625,18 @@
 //             }
 //         }
 // }
+
+public class Start{
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int sum = 0;
+
+            int i=1;
+            while(i<=n){
+                sum +=i;
+                i++;
+            }
+            System.out.println("sum is :"+(sum));
+        }
+}
