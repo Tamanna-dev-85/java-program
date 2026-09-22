63af28c132e4ad9be47c5d0d82680e2fbc754aaf
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-22 21:53:32 +0530

    update code

--- hello.java
+++ hello.java
@@ -751,28 +751,28 @@ import java.util.Scanner;
 //   }
 // }
 
-public class hello{
-  public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      System.out.println("enter a :");
-      int a = sc.nextInt();
-      System.out.println("enter b :");
-      int b = sc.nextInt();
-      char operator = sc.next().charAt(0);
+// public class hello{
+//   public static void main(String[] args) {
+//       Scanner sc = new Scanner(System.in);
+//       System.out.println("enter a :");
+//       int a = sc.nextInt();
+//       System.out.println("enter b :");
+//       int b = sc.nextInt();
+//       char operator = sc.next().charAt(0);
 
-      switch (operator) {
-          case '+':System.out.println(a+b);
-               break;
-          case '-':System.out.println(a-b);
-               break;
-          case '*':System.out.println(a*b);
-             break;
-          case '/':System.out.println(a/b);
-          break;
-          case '%':System.out.println(a%b);
-          break;
-          default:System.out.println("wrong operator");
-     }
+//       switch (operator) {
+//           case '+':System.out.println(a+b);
+//                break;
+//           case '-':System.out.println(a-b);
+//                break;
+//           case '*':System.out.println(a*b);
+//              break;
+//           case '/':System.out.println(a/b);
+//           break;
+//           case '%':System.out.println(a%b);
+//           break;
+//           default:System.out.println("wrong operator");
+//      }
       
-  }
-}
+//   }
+// }
