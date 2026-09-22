2111ce7298d245b5df6a77f5f863f3ba3f40c2ba
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-22 20:19:19 +0530

    update code

--- hello.java
+++ hello.java
@@ -687,16 +687,16 @@ import java.util.Scanner;
 //   }
 // }
 // }
-public class hello{
-  public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      int age = sc.nextInt();
+// public class hello{
+//   public static void main(String[] args) {
+//       Scanner sc = new Scanner(System.in);
+//       int age = sc.nextInt();
 
-      if (age>=18){
-        System.out.println("vote");
-      }
-  else{
-   System.out.println("not voted");
-  }
-}
-}
+//       if (age>=18){
+//         System.out.println("vote");
+//       }
+//   else{
+//    System.out.println("not voted ");
+//   }
+// }
+// }
