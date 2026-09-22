91a266e56333e38a6c8ccd0d24faac0aa0fea923
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-22 21:32:03 +0530

    update code

--- hello.java
+++ hello.java
@@ -733,17 +733,17 @@
 //   }
 // }
 
-public class hello{
-  public static void main(String[] args) {
-      int number = 4;
-      switch(number){
-        case 1:System.out.println("samosa");
-        break;
-        case 2:System.out.println("pizza");
-        break;
-        case 3:System.out.println("burger");
-        break;
-        default:System.out.println("dream");
-      }
-  }
-}
+// public class hello{
+//   public static void main(String[] args) {
+//       int number = 4;
+//       switch(number){
+//         case 1:System.out.println("samosa");
+//         break;
+//         case 2:System.out.println("pizza");
+//         break;
+//         case 3:System.out.println("burger");
+//         break;
+//         default:System.out.println("dream");
+//       }
+//   }
+// }
