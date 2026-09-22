ec42e6f21e0b17a96eb033fd33f44a8fa5d9ca56
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-22 22:50:04 +0530

    update code

--- Start.java
+++ Start.java
@@ -612,3 +612,13 @@
 //     }
 // }
 
+public class Start{
+        public static void main(String[] args) {
+            int counter =0 ;
+
+            while(counter<100){
+                System.err.println("hello world");
+                counter++;
+            }
+        }
+}
