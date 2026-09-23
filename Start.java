69f1bb0aabc2a27393e703efa0f9d489067cbefe
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-23 22:27:51 +0530

    update code

--- Start.java
+++ Start.java
@@ -687,22 +687,22 @@
 //         }
 // }
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
-public class Start{
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-          do { 
-             System.out.print("enter your number: ");
-             int n = sc.nextInt(); 
+// public class Start{
+//         public static void main(String[] args) {
+//             Scanner sc = new Scanner(System.in);
+//           do { 
+//              System.out.print("enter your number: ");
+//              int n = sc.nextInt(); 
 
-             if(n%10==0){
-                continue;
-             }
-          System.out.println("number was:"+n);
+//              if(n%10==0){
+//                 continue;
+//              }
+//           System.out.println("number was:"+n);
 
-        }while (true) ;
+//         }while (true) ;
         
       
-   }
-}
+//    }
+// }
