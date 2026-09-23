92eebd3183636cdc5c89332069a0503a9956b4b3
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-23 22:08:24 +0530

    update code

--- Start.java
+++ Start.java
@@ -665,13 +665,13 @@
 //         }
 // }
 
-public class Start{
-        public static void main(String[] args) {
-               for(int i=1;i<=10;i++){
-                if(i==5){
-                break;
-                }
-                System.out.println("hello");
-               } 
-        }
-}
+// public class Start{
+//         public static void main(String[] args) {
+//                for(int i=1;i<=10;i++){
+//                 if(i==5){
+//                 break;
+//                 }
+//                 System.out.println("hello");
+//                } 
+//         }
+// }
