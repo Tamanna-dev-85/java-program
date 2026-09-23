c9806589f1c114eaba382493569ae72dc240e8df
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-23 16:39:21 +0530

    update code

--- Start.java
+++ Start.java
@@ -647,12 +647,12 @@
 //         }
 // }
 
-public class Start{
-        public static void main(String[]args){
-         int counter=1;
-         while(counter<=10) {
-                System.out.println("number is :"+ (counter) );
-                counter++;
-         }      
-        }
-}
+// public class Start{
+//         public static void main(String[]args){
+//          int counter=1;
+//          while(counter<=10) {
+//                 System.out.println("number is :"+ (counter) );
+//                 counter++;
+//          }      
+//         }
+// }
