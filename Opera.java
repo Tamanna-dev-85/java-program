59cc9f28a9c25f7c1687c0a48ed2c4361b668b09
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-25 23:32:03 +0530

    update code

--- Opera.java
+++ Opera.java
@@ -375,18 +375,18 @@
 //  }
 
 
- public class Opera{
-  public static void main(String[] args) {
-      Scanner sc=new Scanner(System.in);
-      int num = sc.nextInt();
-      if(num%2==0){
-System.out.println("EVEN");
-      }
-      else{
-        System.out.println("ODD");
-      }
-  }
- }
+//  public class Opera{
+//   public static void main(String[] args) {
+//       Scanner sc=new Scanner(System.in);
+//       int num = sc.nextInt();
+//       if(num%2==0){
+// System.out.println("EVEN");
+//       }
+//       else{
+//         System.out.println("ODD");
+//       }
+//   }
+//  }
 
 
 // import java.util.*;
