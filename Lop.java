29059e90d2d552a57e77843bd275e8d9bdda6e84
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-25 23:29:39 +0530

    update code

--- Lop.java
+++ Lop.java
@@ -1,24 +1,24 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class Lop {
-      public static void main (String[]args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a");
-        int a = sc.nextInt();
-        System.out.println("Enter b");
-        int b = sc.nextInt();
-        System.out.println("Enter operator");
-        char operator = sc.next().charAt(0);
+// public class Lop {
+//       public static void main (String[]args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a");
+//         int a = sc.nextInt();
+//         System.out.println("Enter b");
+//         int b = sc.nextInt();
+//         System.out.println("Enter operator");
+//         char operator = sc.next().charAt(0);
 
-        switch(operator){
+//         switch(operator){
 
-            case  '+' -> System.out.println(a+b);
-            case '-' -> System.out.println(a-b);
-            case '*' -> System.out.println(a*b);
-            case '/' -> System.out.println(a/b);
-            default -> System.out.println("invalid");
+//             case  '+' -> System.out.println(a+b);
+//             case '-' -> System.out.println(a-b);
+//             case '*' -> System.out.println(a*b);
+//             case '/' -> System.out.println(a/b);
+//             default -> System.out.println("invalid");
 
-        }
-    }
+//         }
+//     }
     
-}
+// }
