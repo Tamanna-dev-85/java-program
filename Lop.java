1ae3f71da52c7186bb5e885465e7f2f21305cb99
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-26 13:54:31 +0530

    update code

--- Lop.java
+++ Lop.java
@@ -35,7 +35,7 @@ import java.util.Scanner;
 // }else{
 
 //         boolean isPrime = true;
-//         for(int i = 2;i<=n-1;i++){
+//         for(int i = 2;i<=Math.sqrt(n);i++){
 //             if(n%i==0){ // n is a multiple of i(i not equal to 1 or n)
 //                 isPrime = false;
 //             }
