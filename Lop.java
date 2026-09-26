29d7ce246b4f3143d08cba83c5a3c95884560028
Tamanna-dev-85 <tamannaparween907@gmail.com>
2026-09-26 13:51:42 +0530

    update code

--- Lop.java
+++ Lop.java
@@ -25,27 +25,27 @@ import java.util.Scanner;
 //     }
     
 // }
-public class Lop{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+// public class Lop{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
 
-if (n==2){
-    System.out.println("n is prime");
-}else{
+// if (n==2){
+//     System.out.println("n is prime");
+// }else{
 
-        boolean isPrime = true;
-        for(int i = 2;i<=n-1;i++){
-            if(n%i==0){ // n is a multiple of i(i not equal to 1 or n)
-                isPrime = false;
-            }
-        }
+//         boolean isPrime = true;
+//         for(int i = 2;i<=n-1;i++){
+//             if(n%i==0){ // n is a multiple of i(i not equal to 1 or n)
+//                 isPrime = false;
+//             }
+//         }
 
-        if(isPrime == true){
-            System.out.println("n is prime");
-        }else{
-            System.out.println("n is not prime");
-        }
-    }
-}
-}
+//         if(isPrime == true){
+//             System.out.println("n is prime");
+//         }else{
+//             System.out.println("n is not prime");
+//         }
+//     }
+// }
+// }
